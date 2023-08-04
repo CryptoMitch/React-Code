5330a3a55f01427a6633692a5c797936c5586feb
Mitch Hamilton <thecryptomitch@gmail.com>
2023-08-05 06:03:18 +0800

    Mobile Responsive + Color Theme Switcher

--- theme-switcher/src/App.js
+++ theme-switcher/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+  };
+
+  const themeStyles = {
+    light: {
+      backgroundColor: '#F1F1F1',
+      color: '#0F3D3E',
+    },
+    dark: {
+      backgroundColor: '#100F0F',
+      color: '#f4f4f4',
+    },
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app" style={themeStyles[theme]}>
+      <header>
+        <h1>My Unique React App</h1>
+        <button onClick={toggleTheme} className="theme-switcher">
+          Toggle Theme
+        </button>
       </header>
+      <main>
+        <p>Welcome to my unique React app! Play around with the theme switcher to change from dark mode to light mode.</p>
+      </main>
     </div>
   );
 }
