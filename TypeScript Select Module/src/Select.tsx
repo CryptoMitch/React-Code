df356463233c5dc0052d22cf96f6710f3ad09679
Mitch Hamilton <thecryptomitch@gmail.com>
2023-08-08 10:11:06 +0800

    Functionality for Toggle working correctly

--- TypeScript Select Module/src/Select.tsx
+++ TypeScript Select Module/src/Select.tsx
@@ -1,5 +1,7 @@
 // This code is inspired by the Tutorial by Web Dev Simplified - https://www.youtube.com/watch?v=bAJlYgeovlg
+import React, {useState } from 'react';
 import styles from "./select.module.css"
+
 
 // A type to support the value option
 type SelectOption = {
@@ -15,14 +17,25 @@ type SelectProps ={
 }
 
 export function Select({ value, onChange, options }: SelectProps) {
-        // Wrapper for the entire component pulling styles from select.module.css
+    // State variable to control the dropdown open/close state
+    const [isOpen, setIsOpen] = useState(true)
+        // Wrapper for the entire component using styles from select.module.css
         return (
-        <div tabIndex={0} className={styles.container}>
+        <div 
+        // when item loses focus
+        onBlur={() => setIsOpen(false)}
+        //toggle the drop down on click
+            onClick={() => setIsOpen(prev => !prev)}
+            // add keyboard focus for accessibility
+            tabIndex={0} 
+            // use styles
+            className={styles.container}
+        >
           <span className={styles.value}>{value?.label}</span>
           <button className={styles["clear-btn"]}>&times;</button>
           <div className={styles.divider}></div>
           <div className={styles.caret}></div>
-          <ul className={`${styles.options} ${styles.show}`}>
+          <ul className={`${styles.options} ${isOpen ? styles.show : ""}`}>
             {options.map(option => (
                 <li key ={option.label} className={styles.option}>
                     {option.label}
